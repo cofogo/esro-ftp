75714feb1c89aa8b60cb16d488ae378d151bdcc3
Aleksandar Popov <alexander.popov.1505@gmail.com>
2025-09-18 13:24:53 +0300

    fix 3

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -168,7 +168,7 @@ data "aws_route53_zone" "domain" {
 }
 # FOR NOW HERE, SHOULD BE REMOVED
 resource "aws_route53_record" "esro_apex_a" {
-  zone_id = data.aws_route53_zone.esro.zone_id
+  zone_id = data.aws_route53_zone.domain[0].zone_id
   name    = ""
   type    = "A"
   alias {
