2647f88bfc87e33063908f3c27add7b738d504c4
Tomdatbenik <tomvankaathoven@gmail.com>
2025-09-18 16:49:17 +0200

    Update main.tf

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -20,7 +20,12 @@ resource "aws_security_group" "ftp_server" {
     cidr_blocks = var.allowed_cidr_blocks
   }
 
-
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   # FTPS implicit SSL port
   ingress {
